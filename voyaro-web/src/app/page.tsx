3d918f431c9124a3ae4e37fdb74bbf95e8a1df7a
GaBeX <nika.gabedava1@gmail.com>
2026-01-24 00:41:42 +0300

    Match Join as Tour Company button style to Start a Group Trip

--- voyaro-web/src/app/page.tsx
+++ voyaro-web/src/app/page.tsx
@@ -271,7 +271,7 @@ export default function LandingPage() {
               </ul>
               <div className="mb-6 h-5"></div>
               <Link href="/register/company">
-                <Button className="w-full bg-transparent border border-white/30 text-white hover:bg-white/10">{translate(t.landing.joinAsCompany)}</Button>
+                <Button className="w-full bg-white text-gray-900 hover:bg-gray-100">{translate(t.landing.joinAsCompany)}</Button>
               </Link>
             </div>
           </div>
